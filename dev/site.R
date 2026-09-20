00752d845088e37821498e85b6f51a87d1c4fb34
Henry-Osorto <henryantonioosortoruiz@gmail.com>
2026-09-20 14:40:45 -0600

    Update funtions

--- dev/site.R
+++
@@ -1,5 +0,0 @@
-# Local documentation only. Does not deploy to GitHub Pages or open a browser.
-script <- sub("^--file=", "", grep("^--file=", commandArgs(), value = TRUE)[1L])
-package_dir <- normalizePath(file.path(dirname(script), ".."), mustWork = TRUE)
-Sys.unsetenv("BCH_API_KEY")
-pkgdown::build_site(pkg = package_dir, preview = FALSE, new_process = FALSE)
